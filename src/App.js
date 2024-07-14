e5f286ae062b1b85ea3e0ce0cfd5f85f9b4a0c2a
JIN921 <wlslwlsl808@gmail.com>
2024-07-14 20:36:22 +0900

    [Feat]폰트 추가 및 기본 페이지 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import './assets/fonts/fonts.css';
+
+import Attendance from './pages/Attendance';
+import Calendar from './pages/Calendar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 function App() {
-  return <div>Weeth</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/attendacne" element={<Attendance />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,37 @@
+const theme = {
+  color: {
+    main: {
+      mainColor: '##00DDA8',
+      positive: '##508FFF',
+      negative: '##FF5858',
+      pointPurple: '##6F50CB',
+      pointPink: '#D968D3',
+    },
+    grayScale: {
+      black: '#000000',
+      lightBlack: '#1c1a19',
+      darkGray: '#2b2826',
+      gray: '#666666',
+      lightGray: '#F4F4F4',
+      brightGray: '#EFEFEF',
+      brightLightGray: '#F8F8FA',
+      mediumGray: '#8B8B8B',
+      mediumBrightGray: '#9D9D9D',
+      white: '#ffffff',
+      gray12: '#1F1F1F',
+      gray18: '#2F2F2F',
+      gray20: '#4D4D4D',
+      gray30: '#A6A6A6',
+      gray65: '#333333',
+    },
+  },
+
+  font: {
+    family: {
+      pretendard_semiBold: 'Pretendard SemiBold',
+      pretendard_regular: 'Prentendard Regular',
+    },
+  },
+};
+
+export default theme;
