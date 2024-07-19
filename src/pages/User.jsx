ce3d84efdaaeb1b4c31aa9d6e6aef8b5d80f05c1
yezzan9 <rubyyy718@gmail.com>
2024-07-19 16:56:27 +0900

    [Feat] 상세페이지 구현 및 멤버 리스트 매핑

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import MemberHeader from '../components/Header/UserHeader';
+import UserHeader from '../components/Header/UserHeader';
 import Generation from '../components/Header/Generation';
-import MemberName from '../components/UserName';
+import UserName from '../components/UserName';
 
 const StyledMember = styled.div`
   width: 370px;
@@ -15,16 +15,58 @@ const MemberList = styled.div`
   height: 100%;
 `;
 
+const mockUser = [
+  {
+    name: '이강혁',
+    studentId: '1234567',
+    tel: '01012345678',
+    department: 'SW',
+    email: '123@123.123',
+    cardinal: 1,
+    position: 'BE',
+  },
+  {
+    name: '박예진',
+    studentId: '1234567',
+    tel: '01012345678',
+    department: 'SW',
+    email: '123@123.123',
+    cardinal: 2,
+    position: 'FE',
+  },
+  {
+    name: '조예진',
+    studentId: '1234567',
+    tel: '01012345678',
+    department: 'SW',
+    email: '123@123.123',
+    cardinal: 3,
+    position: 'D',
+  },
+  {
+    name: '김성민',
+    studentId: '1234567',
+    tel: '01012345678',
+    department: 'SW',
+    email: '123@123.123',
+    cardinal: 3,
+    position: 'MA',
+  },
+];
+
 const Member = () => {
   return (
     <StyledMember>
-      <MemberHeader />
+      <UserHeader mockUser={mockUser} />
       <Generation />
       <MemberList>
-        <MemberName />
-        <MemberName />
-        <MemberName />
-        <MemberName />
+        {mockUser.map((user) => (
+          <UserName
+            name={user.name}
+            cardinal={user.cardinal}
+            position={user.position}
+          />
+        ))}
       </MemberList>
     </StyledMember>
   );
