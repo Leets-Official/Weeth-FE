034184d8f4e8875f44071e6a1f518cb8ac03b891
JIN921 <wlslwlsl808@gmail.com>
2024-07-21 05:45:16 +0900

    [design] 출석, 패널티 모달 css 적용

--- src/components/Attendance/Modal/ModalAttend.jsx
+++ src/components/Attendance/Modal/ModalAttend.jsx
@@ -2,39 +2,77 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ModalAttend.css';
 import styled from 'styled-components';
+import MiddleButton from '../../Button/MiddleButton';
+import theme from '../../../styles/theme';
+import icClose from '../../../assets/images/ic_close.png';
+import check from '../../../assets/images/ic_check.png';
 
 const Line = styled.div`
-  border: 1px solid;
-  margin: 10px;
+  border: 1px solid #4d4d4d;
+  margin: 30px 10px 0px 10px;
   transform: scaleY(0.2);
 `;
+const Regular = styled.div`
+  font-family: ${theme.font.family.pretendard_regular};
+`;
+const SemiBold = styled.div`
+  font-family: ${theme.font.family.pretendard_semiBold};
+  include-font-padding: false;
+  display: flex;
+  flex-direction: row;
+`;
+const ImgButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+`;
+const CloseButton = ({ onClick }) => {
+  return (
+    <ImgButton onClick={onClick}>
+      <img src={icClose} alt="X" />
+    </ImgButton>
+  );
+};
+
+CloseButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 const ModalAttend = ({ open, close }) => {
   return (
     <div className={open ? 'openModal modal' : 'modal'}>
       {open && (
-        <div className="modal-content">
-          <div className="modal-header">
-            <h2>출석하기</h2>
-            <button className="modal-close" onClick={close} type="button">
-              &times;
-            </button>
+        <Regular>
+          <div className="modal-content">
+            <div className="modal-header">
+              <img src={check} alt="V" className="modal-check-icon" />
+              <CloseButton onClick={close} />
+            </div>
+            <div className="modal-body">
+              <SemiBold className="modal-title">출석하기</SemiBold>
+              <SemiBold className="modal-text">
+                오늘은&nbsp;
+                <div style={{ color: theme.color.main.mainColor }}>
+                  프로젝트 중간 발표
+                </div>
+                &nbsp;가 있는 날이에요
+              </SemiBold>
+              <div className="modal-date">
+                날짜: 2024년 7월 18일 (19:00 - 20:30)
+              </div>
+              <div className="modal-place">장소: 가천관 247호</div>
+              <Line />
+              <input
+                className="modal-input"
+                type="text"
+                placeholder="코드를 입력하세요"
+              />
+            </div>
+            <div className="modal-buttons">
+              <MiddleButton onClick={close}>입력완료</MiddleButton>
+            </div>
           </div>
-          <div className="modal-body">
-            <p>
-              오늘은 <strong>“프로젝트 중간 발표”</strong>가 있는 날이에요
-            </p>
-            <p>날짜: 2024년 7월 18일 (19:00 - 20:30)</p>
-            <p>장소: 가천관 247호</p>
-            <Line />
-            <input className="modal-input" type="text" placeholder="I" />
-          </div>
-          <div className="modal-buttons">
-            <button onClick={close} type="button">
-              입력완료
-            </button>
-          </div>
-        </div>
+        </Regular>
       )}
     </div>
   );
