f0d176d42f0b652eeca2aa6b7317da04ae42ff79
yezzan9 <rubyyy718@gmail.com>
2024-07-20 23:44:24 +0900

    [design] 폰트 단위 변경

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledText = styled.div`
-  font-size: 18pt;
+  font-size: 18px;
   font-weight: 600; //semi-bold
 `;
 
