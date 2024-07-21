975757569d69886ac9c6baa6289dca1528b85491
yezzan9 <rubyyy718@gmail.com>
2024-07-22 03:15:00 +0900

    [feat] 캘린더(년) 매핑 및 css 수정

--- src/components/Calendar/MonthlyEvent.jsx
+++ src/components/Calendar/MonthlyEvent.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import theme from '../../styles/theme';
 
 const StyledYear = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
 `;
 
 const MonthlyContent = styled.div`
   display: flex;
   background-color: #2e2e2e;
-  padding: 20px;
-  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  width: 143px; //사이즈 수정 필요
   border-radius: 20px;
-  margin: 10px;
+  font-size: 14px;
 `;
 
-const MonthlyEvent = () => {
+const MonthName = styled.div`
+  padding-left: 10px;
+  padding-bottom: 7px;
+  color: ${(props) => (props.isToday ? '#00dda8' : '#ffffff')};
+  font-size: 18px;
+  font-family: ${theme.font.family.pretendard_semiBold};
+`;
+
+const MonthlyEvent = ({ month, todayMonth }) => {
+  const isToday = month === todayMonth;
   return (
     <StyledYear>
-      <h3>1월</h3>
+      <MonthName isToday={isToday}>{month}월</MonthName>
       <MonthlyContent>
         첫번째 일정
         <br />
@@ -30,14 +41,9 @@ const MonthlyEvent = () => {
   );
 };
 
-// MonthlyEvent.propTypes = {
-//   mockEventYear: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       title: PropTypes.string.isRequired,
-//       start: PropTypes.string.isRequired,
-//       end: PropTypes.string.isRequired,
-//     }),
-//   ).isRequired,
-// };
+MonthlyEvent.propTypes = {
+  month: PropTypes.number.isRequired,
+  todayMonth: PropTypes.number.isRequired,
+};
 
 export default MonthlyEvent;
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -8,7 +8,6 @@ import ToggleButton from '../components/Calendar/ToggleButton';
 
 const StyledCalendar = styled.div`
   width: 370px;
-  height: 800px;
 `;
 
 const Content = styled.div`
@@ -24,6 +23,9 @@ const mockEvent = [
   { title: '몇주에 걸친 일정', start: '2024-06-30T19:00:00.000', end: '2024-08-01T19:00:00.000'},
 ];
 
+const todayYear = new Date().getFullYear();
+const todayMonth = new Date().getMonth() + 1;
+
 const Calendar = () => {
 
   const [calendarType, setCalenderType] = useState('month');
@@ -34,10 +36,10 @@ const Calendar = () => {
 
   return (
     <StyledCalendar>
-      <CalendarHeader />
+      <CalendarHeader todayMonth={todayMonth} todayYear={todayYear} />
         <Content>
         <ToggleButton onToggle={onToggle} />
-        {calendarType === 'month' ? <MonthCalendar mockEvent={mockEvent}/> : <YearCalendar />}
+        {calendarType === 'month' ? <MonthCalendar mockEvent={mockEvent}/> : <YearCalendar todayMonth={todayMonth}/>}
         {/* <button type="button" onClick={() => changeCalenderType('month')}>month</button>
         <button type="button" onClick={() => changeCalenderType('year')}>year</button> */}
       </Content>
