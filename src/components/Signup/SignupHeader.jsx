539b7d2fd1c58bd3eb6e020a35a5c5f2554992b0
hyewonimdang <hyehyekuk202@gmail.com>
2024-07-22 04:19:37 +0900

    [design] 메일 입력 페이지 렌더링

---
+++ src/components/Signup/SignupHeader.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import LeftButton from '../Header/LeftButton';
+// import RightButton from '../Header/RightButton';
+import TextButton from '../Header/TextButton';
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 45px 25px 20px 25px; //기본 헤더 마진
+`;
+
+/* const TitleWrapper = styled.div`
+  // position: absolute;
+  // left: 50%;
+  // transform: translateX(-50%);
+`; */
+
+/* const NextButton = styled.button`
+  color: ${({ disabled }) => (disabled ? 'white' : '#00DDA8')};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+`;;   */
+
+const SignupHeader = ({
+  onClickLeftButton,
+  isRightButtonEnabled,
+  onClickTextButton,
+  nextButtonText,
+}) => {
+  return (
+    <StyledHeader>
+      <LeftButton onClick={onClickLeftButton} />
+
+      <TextButton
+        onClick={onClickTextButton}
+        disabled={!isRightButtonEnabled}
+        text={nextButtonText}
+        style={{ color: isRightButtonEnabled ? '#00DDA8' : '#white' }}
+      />
+    </StyledHeader>
+  );
+};
+
+SignupHeader.propTypes = {
+  onClickLeftButton: PropTypes.func.isRequired,
+  isRightButtonEnabled: PropTypes.bool.isRequired,
+  onClickTextButton: PropTypes.func.isRequired,
+  nextButtonText: PropTypes.string.isRequired,
+};
+
+export default SignupHeader;
