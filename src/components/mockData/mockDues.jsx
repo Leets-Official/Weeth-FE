b2135161bf83ef86cac25bccabb03a782ac370d7
JIN921 <wlslwlsl808@gmail.com>
2024-07-22 17:42:57 +0900

    [feat] mockDues 추가

---
+++ src/components/mockData/mockDues.jsx
@@ -0,0 +1,33 @@
+const mockDues = [
+  {
+    // cardinal 1이 수입 2가 지출
+    dues: '234,234',
+    date: '2024. 03',
+    cardinal: 1,
+    memo: '',
+  },
+  {
+    dues: '234,234',
+    date: '2024. 03',
+    cardinal: 1,
+    memo: '메모',
+  },
+  {
+    dues: '234,234',
+    date: '2024. 03',
+    cardinal: 2,
+    memo: '20,000 * 12명',
+  },
+  {
+    dues: '234,234',
+    date: '2024. 04',
+    cardinal: 2,
+    memo: '',
+  },
+  {
+    dues: '234,234',
+    date: '2024. 05',
+    cardinal: 2,
+    memo: '',
+  },
+];
