97330f20e9d3230898a1a419b292e1b4374f3f9b
JIN921 <wlslwlsl808@gmail.com>
2024-07-22 21:07:20 +0900

    [fix] 회비 페이지에 맞게 컴포넌트 수정

--- src/pages/Dues.jsx
+++ src/pages/Dues.jsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-
 import theme from '../styles/theme';
 import DuesHeader from '../components/Dues/DuesHeader';
 import DueCategory from '../components/Dues/DueCategory';
-import DuesName from '../components/Dues/DuesName';
+import DuesInfo from '../components/Dues/DuesInfo';
 import mockDues from '../components/mockData/mockDues';
 import DuesTitle from '../components/Dues/DuesTitle';
 
 const StyledDues = styled.div`
   width: 370px;
+  height: height: calc(var(--vh, 1vh) * 100);
   font-family: ${theme.font.family.pretendard_regular};
 `;
 
@@ -17,14 +17,42 @@ const CategoryWrapper = styled.div`
   margin: 0 30px;
 `;
 
-const DuesList = styled.div`
-  position: absolute;
-  background-color: #2e2e2e;
+const DuesListBox = styled.div`
+  width: 92%;
+  height: calc(var(--vh, 1vh) * 100);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  height: 100%;
-  max-width: 350px;
-  margin: 0px 10px;
+  margin: 0 4%;
+  background-color: ${theme.color.grayScale.gray18};
+`;
+
+const DuesList = styled.div`
+  width: 92%;
+  margin: 20px 10px 0 10px;
+`;
+
+const Line = styled.div`
+  border: 1px solid;
+  color: #4d4d4d;
+  width: 325px;
+  margin-top: 20px;
+  transform: scaleY(0.2);
+`;
+
+const MoneyBoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 35px;
+`;
+
+const MoneyBox = styled.div`
+  font-size: 32px;
+  font-family: ${theme.font.family.pretendard_semiBold};
+  margin-left: 10px;
 `;
 
 const Dues = () => {
@@ -42,16 +70,23 @@ const Dues = () => {
       <CategoryWrapper>
         <DueCategory setSelectedCardinal={setSelectedCardinal} />
       </CategoryWrapper>
-      <DuesList>
-        {filteredDues.map((dues) => (
-          <DuesName
-            key={dues.name}
-            name={dues.name}
-            cardinal={dues.cardinal}
-            position={dues.position}
-          />
-        ))}
-      </DuesList>
+      <DuesListBox>
+        <MoneyBoxContainer>
+          <MoneyBox>234,234원</MoneyBox>
+        </MoneyBoxContainer>
+        <Line />
+        <DuesList>
+          {filteredDues.map((dues) => (
+            <DuesInfo
+              key={dues.id}
+              dues={dues.dues}
+              cardinal={dues.cardinal}
+              date={dues.date}
+              memo={dues.memo}
+            />
+          ))}
+        </DuesList>
+      </DuesListBox>
     </StyledDues>
   );
 };
