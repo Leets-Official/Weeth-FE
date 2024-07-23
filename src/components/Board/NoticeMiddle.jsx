b5dcfc4d06b4164303fc7540bd3785f5f7b4a493
hyewonimdang <hyehyekuk202@gmail.com>
2024-07-23 14:50:22 +0900

    [design] 글쓰기 버튼 UI 구현

--- src/components/Board/NoticeMiddle.jsx
+++ src/components/Board/NoticeMiddle.jsx
@@ -5,49 +5,63 @@ import theme from '../../styles/theme';
 
 const NoticeMiddleContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  justify-content: space-between;
+  align-items: center;
+  width: auto;
+  padding: 0 7%;
+  margin-bottom: 20px;
 `;
 
 const NoticeTextContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 4% 15px 10px 6%;
+  align-items: flex-start;
+  width: calc(100% - 72px);
 `;
 
 const NoticeTitle = styled.div`
-  width: 74%;
+  margin-top: 20px;
   color: ${theme.color.grayScale.white};
-  font-family: ${theme.font.family.pretendard_semiBold};
+  font-family: ${theme.font.family.Pretendard_SemiBold};
   font-weight: 600;
   font-size: 16px;
   line-height: 19.09px;
 `;
 
-/* 여기서부터 */
 const NoticeIntroduce = styled.div`
+  width: 100%;
   color: ${theme.color.grayScale.gray65};
-  font-family: ${theme.font.family.pretendard_regular};
+  font-family: ${theme.font.family.Pretendard_regular};
   font-weight: 400;
   font-size: 12px;
   margin-top: 10px;
 `;
 
-const NoticeMiddle = ({ title }) => {
+const StyledButton = styled.div`
+  margin-top: 20px;
+  margin-left: -50px;
+`;
+
+const NoticeMiddle = ({ title, description, button }) => {
   return (
     <NoticeMiddleContainer>
       <NoticeTextContainer>
         <NoticeTitle>{title}</NoticeTitle>
-        <NoticeIntroduce>
-          자세한 내용을 보려면 게시물을 클릭하세요...
-        </NoticeIntroduce>
+        <NoticeIntroduce>{description}</NoticeIntroduce>
       </NoticeTextContainer>
+      <StyledButton>{button}</StyledButton>
     </NoticeMiddleContainer>
   );
 };
 
 NoticeMiddle.propTypes = {
   title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  button: PropTypes.element,
+};
+
+NoticeMiddle.defaultProps = {
+  button: null,
 };
 
 export default NoticeMiddle;
