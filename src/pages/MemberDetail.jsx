404375b412512c6efc103c88f5dd6d37e3ce21c2
yezzan9 <rubyyy718@gmail.com>
2024-07-25 21:38:29 +0900

    [refactor] 멤버 정보 컴포넌트화

--- src/pages/MemberDetail.jsx
+++ src/pages/MemberDetail.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+
 import { useLocation } from 'react-router-dom';
 import MemberHeader from '../components/Member/MemberHeader';
-// import mockUser from '../components/mockData/mockUser';
+import InfoComponent from '../components/Member/InfoComponent';
 
 import icName from '../assets/images/ic_name.svg';
 import icId from '../assets/images/ic_studentID.svg';
@@ -10,40 +11,14 @@ import icCardinal from '../assets/images/ic_cardinal.svg';
 import icPosition from '../assets/images/ic_position.svg';
 import icEmail from '../assets/images/ic_mail.svg';
 
-import theme from '../styles/theme';
-
 const StyledDetails = styled.div`
   width: 370px;
   // 해당페이지는 컴포넌트화될 예정이므로 따로 추가하지 않음
   // 추후에 만들어질 컴포넌트에서 설정하겟습니당
 `;
 
-const Line = styled.div`
-  border: 1px solid;
-  width: 325px;
-  transform: scaleY(0.2);
-  margin: auto;
-`;
-
 const InfoWrapper = styled.div`
   padding-top: 20px;
-`;
-
-const Info = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 25px;
-`;
-
-const Text = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-left: 10px;
-`;
-
-const MainColor = styled.div`
-  color: ${theme.color.main.mainColor};
 `;
 
 const UserDetail = () => {
@@ -51,77 +26,31 @@ const UserDetail = () => {
   const { name, studentId, department, email, cardinal, position } =
     location.state;
 
-  let positionKo = 'none';
-
-  switch (position) {
-    case 'FE':
-      positionKo = '프론트엔드';
-      break;
-    case 'BE':
-      positionKo = '백엔드';
-      break;
-    case 'D':
-      positionKo = '디자인';
-      break;
-    case 'MA':
-      positionKo = '임원';
-      break;
-    default:
-      positionKo = '없음';
-  }
-
   return (
     <StyledDetails>
       <MemberHeader />
       <InfoWrapper>
-        <Info>
-          <img src={icName} alt="name" />
-          <Text>
-            <div>이름</div>
-            <MainColor>{name}</MainColor>
-          </Text>
-        </Info>
-        <Line />
-        <Info>
-          <img src={icId} alt="id" />
-          <Text>
-            <div>학번</div>
-            <MainColor>{studentId}</MainColor>
-          </Text>
-        </Info>
-        <Line />
-        <Info>
-          <img src={icDepartment} alt="department" />
-          <Text>
-            <div>학과</div>
-            <MainColor>{department}</MainColor>
-          </Text>
-        </Info>
-        <Line />
-        <Info>
-          <img src={icCardinal} alt="cardinal" />
-          <Text>
-            <div>기수</div>
-            <MainColor>{cardinal}</MainColor>
-          </Text>
-        </Info>
-        <Line />
-        <Info>
-          <img src={icPosition} alt="positon" />
-          <Text>
-            <div>역할</div>
-            <MainColor>{positionKo}</MainColor>
-          </Text>
-        </Info>
-        <Line />
-        <Info>
-          <img src={icEmail} alt="email" />
-          <Text>
-            <div>메일</div>
-            <MainColor>{email}</MainColor>
-          </Text>
-        </Info>
-        <Line />
+        <InfoComponent src={icName} alt="smile" index="이름" value={name} />
+        <InfoComponent src={icId} alt="heart" index="학번" value={studentId} />
+        <InfoComponent
+          src={icDepartment}
+          alt="pencil"
+          index="학과"
+          value={department}
+        />
+        <InfoComponent
+          src={icCardinal}
+          alt="tag"
+          index="기수"
+          value={cardinal}
+        />
+        <InfoComponent
+          src={icPosition}
+          alt="monitor"
+          index="역할"
+          value={position}
+        />
+        <InfoComponent src={icEmail} alt="mail" index="메일" value={email} />
       </InfoWrapper>
     </StyledDetails>
   );
