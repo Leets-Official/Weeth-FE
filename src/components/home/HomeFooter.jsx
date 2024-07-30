bffe73dda3a031c178cd0d74a70c1be88b7b1f7a
JIN921 <wlslwlsl808@gmail.com>
2024-07-31 05:20:56 +0900

    [feat] from leets 그리드 아이템 아이콘 추가

--- src/components/home/HomeFooter.jsx
+++ src/components/home/HomeFooter.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import theme from '../../styles/theme';
+import leets from '../../assets/images/ic_leets.svg';
+import insta from '../../assets/images/ic_insta.svg';
+import discord from '../../assets/images/ic_discord.svg';
+import github from '../../assets/images/ic_github.svg';
 
 const StyledHomeFooter = styled.div`
   font-family: ${theme.font.family.pretendard_semiBold};
@@ -24,21 +28,25 @@ const ScrollContainer = styled.div`
   &::-webkit-scrollbar-thumb:hover {
   }
 `;
+const ImgContainer = styled.div`
+  margin-bottom: 5px;
+`;
 
 const GridItem = styled.div`
   flex: 0 0 auto;
   margin-right: 10px;
-  padding: 10px 20px;
+  padding: 10px 15px 15px 0;
   background-color: ${({ color }) => color || theme.color.grayScale.gray18};
   font-family: ${theme.font.family.pretendard_semiBold};
   width: 34%;
-  height: 87px;
+  height: 77px;
   color: #fff;
   border-radius: 10px;
   display: flex;
+  flex-direction: column;
   justify-content: flex-end;
   align-items: flex-end;
-  font-size: 14px;
+  font-size: 16px;
   white-space: nowrap;
   &:last-child {
     margin-right: 0;
@@ -50,10 +58,30 @@ const HomeFooter = () => {
     <>
       <StyledHomeFooter>From Leets</StyledHomeFooter>
       <ScrollContainer>
-        <GridItem>홈페이지</GridItem>
-        <GridItem>Instagram</GridItem>
-        <GridItem>Discord</GridItem>
-        <GridItem>Github</GridItem>
+        <GridItem>
+          <ImgContainer>
+            <img src={leets} alt="leets" />
+          </ImgContainer>
+          홈페이지
+        </GridItem>
+        <GridItem>
+          <ImgContainer>
+            <img src={insta} alt="instagram" />
+          </ImgContainer>
+          Instagram
+        </GridItem>
+        <GridItem>
+          <ImgContainer>
+            <img src={discord} alt="leediscordts" />
+          </ImgContainer>
+          Discord
+        </GridItem>
+        <GridItem>
+          <ImgContainer>
+            <img src={github} alt="github" />
+          </ImgContainer>
+          Github
+        </GridItem>
       </ScrollContainer>
       <StyledHomeFooter>Leets의 프로젝트를 둘러보세요!</StyledHomeFooter>
       <ScrollContainer>
