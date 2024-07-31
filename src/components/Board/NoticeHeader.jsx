005d7fc6dee613131ece2a5ef0654e5218d70fc4
hyewonimdang <hyehyekuk202@gmail.com>
2024-08-01 00:21:34 +0900

    [feat] 모달 EditDelModal로 수정

--- src/components/Board/NoticeHeader.jsx
+++ src/components/Board/NoticeHeader.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import LeftButton from '../Header/LeftButton';
 import IndexButton from '../Header/IndexButton';
 import Title from '../Header/Title';
-import theme from '../../styles/theme';
+// import theme from '../../styles/theme';
+import EditDelModal from '../EditDelModal';
 
 const StyledHeader = styled.div`
   display: flex;
@@ -15,59 +16,59 @@ const StyledHeader = styled.div`
 
 const TitleWrapper = styled.div``;
 
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+// const ModalOverlay = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   background: rgba(0, 0, 0, 0.5);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-const ModalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-`;
+// const ModalContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   gap: 10px;
+// `;
 
-const ModalContent = styled.div`
-  width: 360px;
-  background: #333333;
-  border-radius: 10px;
-  padding: 10px 0;
-  text-align: center;
-`;
+// const ModalContent = styled.div`
+//   width: 360px;
+//   background: #333333;
+//   border-radius: 10px;
+//   padding: 10px 0;
+//   text-align: center;
+// `;
 
-const MenuTitle = styled.div`
-  font-family: ${theme.font.family.pretendard_regular};
-  padding: 10px 0;
-  color: #fff;
-  cursor: default;
-  border-bottom: 1px solid #444;
-`;
+// const MenuTitle = styled.div`
+//   font-family: ${theme.font.family.pretendard_regular};
+//   padding: 10px 0;
+//   color: #fff;
+//   cursor: default;
+//   border-bottom: 1px solid #444;
+// `;
 
-const MenuItem = styled.div`
-  padding: 15px 0;
-  color: #4886f4;
-  cursor: pointer;
-  &:not(:last-child) {
-    border-bottom: 1px solid #444;
-  }
-`;
+// const MenuItem = styled.div`
+//   padding: 15px 0;
+//   color: #4886f4;
+//   cursor: pointer;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid #444;
+//   }
+// `;
 
-const CancelButton = styled.div`
-  background: #4d4d4d;
-  border-radius: 10px;
-  padding: 15px 0;
-  width: 250px;
-  text-align: center;
-  color: #ffffff;
-  cursor: pointer;
-`;
+// const CancelButton = styled.div`
+//   background: #4d4d4d;
+//   border-radius: 10px;
+//   padding: 15px 0;
+//   width: 250px;
+//   text-align: center;
+//   color: #ffffff;
+//   cursor: pointer;
+// `;
 
 const NoticeHeader = ({ onMenuClick, showModal }) => {
   const [isModalOpen, setIsModalOpen] = useState(showModal);
@@ -84,10 +85,10 @@ const NoticeHeader = ({ onMenuClick, showModal }) => {
     setIsModalOpen(false);
   };
 
-  const handleMenuClickInternal = (action) => {
+  /* const handleMenuClickInternal = (action) => {
     onMenuClick(action); // 여기에서 onMenuClick을 호출합니다.
     setIsModalOpen(false);
-  };
+  }; */
 
   return (
     <>
@@ -99,20 +100,12 @@ const NoticeHeader = ({ onMenuClick, showModal }) => {
         <IndexButton onClick={handleIndexButtonClick} />
       </StyledHeader>
       {isModalOpen && (
-        <ModalOverlay onClick={handleCloseModal}>
-          <ModalContainer onClick={(e) => e.stopPropagation()}>
-            <ModalContent>
-              <MenuTitle>글메뉴</MenuTitle>
-              <MenuItem onClick={() => handleMenuClickInternal('edit')}>
-                수정
-              </MenuItem>
-              <MenuItem onClick={() => handleMenuClickInternal('delete')}>
-                삭제
-              </MenuItem>
-            </ModalContent>
-            <CancelButton onClick={handleCloseModal}>취소</CancelButton>
-          </ModalContainer>
-        </ModalOverlay>
+        <EditDelModal
+          title="글"
+          onClickEdit={() => onMenuClick('edit')}
+          onClickDel={() => onMenuClick('delete')}
+          onClickCancel={handleCloseModal}
+        />
       )}
     </>
   );
