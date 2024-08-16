923d23685a2bab8bacbb93d06ce24a716b0aaee2
JIN921 <wlslwlsl808@gmail.com>
2024-08-17 01:47:03 +0900

    [hotfix]홈 처음 들어왔을 때 이름 error로 뜨는 거 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable react/no-children-prop */
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +9,8 @@ import { ReactComponent as ToggleVisibleIcon } from '../assets/images/ic_toggleV
 import { ReactComponent as ToggleInvisibleIcon } from '../assets/images/ic_toggleInvisible.svg';
 import Utils from '../hooks/Utils';
 import useCustomBack from '../router/useCustomBack';
+import UserAPI from '../hooks/UserAPI';
+import theme from '../styles/theme';
 
 const Container = styled.div`
   display: flex;
@@ -27,7 +30,7 @@ const TextMargin = styled.div`
 
 const ErrorMessage = styled.div`
   right: 0;
-  color: #ff5858;
+  color: ${theme.color.main.negative};
   margin: 15px 0 0 -5%;
   font-size: 14px;
   text-align: right;
@@ -42,15 +45,15 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [error, setError] = useState(null);
+  // UserAPI 완료 상태 관리
+  const [userApiCompleted, setUserApiCompleted] = useState(false);
 
-  const validateEmail = (email) => {
-    return email
-      .toLowerCase()
-      .match(/([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/);
+  const validateEmail = (vaildEmail) => {
+    return vaildEmail.toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
   };
 
-  const validatePwd = (password) => {
-    return password.length >= 8;
+  const validatePwd = (pw) => {
+    return pw.length >= 8;
   };
 
   const isEmailValid = email && validateEmail(email);
@@ -63,63 +66,90 @@ const Login = () => {
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+    const emailValue = e.target.value.replace(/[^a-zA-Z0-9@._-]/g, '');
+    setEmail(emailValue);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
 
- const handleLogin = async (e) => {
-  e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-  if(email===''){
-    alert('이메일을 입력해 주세요.');
-    return;
-  }
-  if(password===''){
-    alert('비밀번호를 입력해 주세요.');
-    return;
-  }
-  const params = {
-    email: email,
-    password: password,
+    if (email === '') {
+      alert('이메일을 입력해 주세요.');
+      return;
+    }
+    if (!validateEmail(email)) {
+      alert('형식에 맞지 않는 이메일입니다.');
+      return;
+    }
+    if (password === '') {
+      alert('비밀번호를 입력해 주세요.');
+      return;
+    }
+    const params = {
+      email,
+      password,
+    };
+
+    try {
+      const BASE_URL = process.env.REACT_APP_BASE_URL;
+      const response = await axios.post(`${BASE_URL}/api/v1/login`, params);
+
+      const validatedResponse = await Utils(
+        response,
+        axios.post,
+        [params],
+        navigate,
+      );
+
+      if (validatedResponse.status === 200) {
+        setError(null);
+        const newToken = response.headers.authorization;
+        const newRefreshToken = response.headers['authorization-refresh'];
+        localStorage.setItem('accessToken', newToken);
+        localStorage.setItem('refreshToken', newRefreshToken);
+        console.log('login token', newToken, newRefreshToken);
+
+        setUserApiCompleted(true); // UserAPI 완료 상태 설정
+      }
+    } catch (err) {
+      console.error('Error:', err);
+
+      if (err.response) {
+        setError(err.response.data); // 서버에서 제공하는 오류 메시지를 설정
+      } else if (err.request) {
+        setError('서버로부터 응답을 받지 못했습니다.');
+      } else {
+        setError('요청을 설정하는 중 오류가 발생했습니다.');
+      }
+    }
   };
 
-  try {
-    const BASE_URL = process.env.REACT_APP_BASE_URL;
-    const response = await axios.post(`${BASE_URL}/api/v1/login`, params);
-
-    const validatedResponse = await Utils(response, axios.post, [params], navigate);
-
-    if (validatedResponse.status === 200) {
-      setError(null);
-      const newToken = response.headers['authorization'];
-      const newRefreshToken = response.headers['authorization-refresh'];
-      localStorage.setItem('accessToken', newToken);
-      localStorage.setItem('refreshToken', newRefreshToken);
-      console.log('login token', newToken, newRefreshToken);
-      
-      // 토큰 저장이 완료된 후에 navigate 호출
+  useEffect(() => {
+    if (userApiCompleted) {
+      // UserAPI 완료 후 홈으로 이동
       navigate('/home');
     }
-  } catch (err) {
-    console.error('Error:', err);
-
-    if (err.response) {
-      setError(err.response.data); // 서버에서 제공하는 오류 메시지를 설정
-    } else if (err.request) {
-      setError('서버로부터 응답을 받지 못했습니다.');
-    } else {
-      setError('요청을 설정하는 중 오류가 발생했습니다.');
-    }
-  }
-};
+  }, [userApiCompleted, navigate]);
 
   return (
     <Container>
-      <LoginHeader isRightButtonEnabled={!!isAllValid} onCompleteClick={handleLogin} />
+      <LoginHeader
+        isRightButtonEnabled={!!isAllValid}
+        onCompleteClick={handleLogin}
+      />
       <LoginHeaderMargin />
-      <SignupTextComponent text="email" value={email} onChange={handleEmailChange} placeholder="ex) weeth@gmail.com" type="text" children='' />
+      <SignupTextComponent
+        text="email"
+        value={email}
+        onChange={handleEmailChange}
+        placeholder="ex) weeth@gmail.com"
+        type="text"
+        children=""
+      />
       <TextMargin />
       <SignupTextComponent
         text="password"
@@ -150,10 +180,8 @@ const Login = () => {
           />
         )}
       </SignupTextComponent>
-      <ErrorMessage>
-        {error}
-      </ErrorMessage>
-      
+      <ErrorMessage>{error}</ErrorMessage>
+      {userApiCompleted && <UserAPI />}
     </Container>
   );
 };
