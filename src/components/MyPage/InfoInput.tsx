decf8923adf13dec8ce945cbd867c76ca857ce2d
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-10-01 01:35:06 +0900

    [refactor] ts 설정 추가 및 InfoInput jsx -> tsx로 변경

--- src/components/MyPage/InfoInput.tsx
+++ src/components/MyPage/InfoInput.tsx
@@ -1,12 +1,22 @@
-/* eslint-disable react/require-default-props */
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import theme from '../../styles/theme';
+import theme from '@/styles/theme';
 import icVisible from '../../assets/images/ic_toggleVisible.svg';
 import icInvisible from '../../assets/images/ic_toggleInvisible.svg';
 
-const StyledInfoInput = styled.div`
+interface InfoInputProps {
+  text?: string;
+  origValue: string | string[];
+  editValue: (val: string) => void;
+  placeholder?: string;
+  width?: string;
+  padding: string;
+  align: string;
+  edit?: boolean;
+  inputType?: 'text' | 'number' | 'no-korean' | 'eng-num';
+}
+
+const StyledInfoInput = styled.div<{ $padding: string }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -19,7 +29,7 @@ const StyledInfoInput = styled.div`
   font-size: 16px;
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ width?: string; $edit?: boolean; $align?: string }>`
   width: ${(props) => props.width || '100%'};
   height: 45px;
   outline: none;
@@ -29,7 +39,6 @@ const Input = styled.input`
   color: ${(props) => (props.$edit ? theme.color.grayScale.gray30 : 'white')};
   padding-left: 10px;
   padding-right: 10px;
-  text-align: ${(props) => props.$align || 'right'};
   font-family: ${theme.font.family.pretendard_regular};
   font-size: 16px;
 
@@ -38,7 +47,7 @@ const Input = styled.input`
   }
 `;
 
-const PwInput = styled.input`
+const PwInput = styled.input<{ width?: string; align?: string }>`
   width: ${(props) => props.width || '100%'};
   height: 45px;
   outline: none;
@@ -63,7 +72,7 @@ const Visible = styled.img`
   cursor: pointer;
 `;
 
-const InfoInput = ({
+const InfoInput: React.FC<InfoInputProps> = ({
   text,
   origValue,
   editValue,
@@ -77,33 +86,33 @@ const InfoInput = ({
   const [value, setValue] = useState(origValue);
   const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const validateValue = (val) => {
-    if (val === '') return true; // 입력값이 비어있을 경우 유효하다고 판단
+  const validateValue = (val: string): boolean => {
+    if (val === '') return true;
     const numberRegex = /^[0-9]*$/;
-    const koreanRegex = /^[ㄱ-ㅎ가-힣]*$/; // 한글만 허용하는 정규 표현식
-    const engNumRegex = /^[a-zA-Z0-9]*$/; // 영어와 숫자만 허용하는 정규 표현식
+    const koreanRegex = /^[ㄱ-ㅎ가-힣]*$/;
+    const engNumRegex = /^[a-zA-Z0-9]*$/;
 
     switch (inputType) {
       case 'text':
-        return koreanRegex.test(val) && val.length <= 5; // 한글만, 최대 5자리
+        return koreanRegex.test(val) && val.length <= 5;
       case 'number':
         if (text === '학번') {
-          return numberRegex.test(val) && val.length <= 9; // 숫자만, 최대 9자리
+          return numberRegex.test(val) && val.length <= 9;
         }
         if (text === '핸드폰') {
-          return numberRegex.test(val) && val.length <= 11; // 숫자만, 최대 11자리
+          return numberRegex.test(val) && val.length <= 11;
         }
         return numberRegex.test(val);
       case 'no-korean':
-        return !koreanRegex.test(val); // 한글 포함하지 않음
+        return !koreanRegex.test(val);
       case 'eng-num':
-        return engNumRegex.test(val); // 영어와 숫자만 허용
+        return engNumRegex.test(val);
       default:
         return true;
     }
   };
 
-  const onChangeValue = (e) => {
+  const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.target.value;
     if (validateValue(val)) {
       setValue(val);
@@ -128,7 +137,7 @@ const InfoInput = ({
           value={value}
           onChange={onChangeValue}
           width={width}
-          $align={align}
+          align={align}
           type={passwordVisible ? 'text' : 'password'}
         />
         {passwordVisible ? (
@@ -157,23 +166,10 @@ const InfoInput = ({
         width={width}
         $align={align}
         $edit={edit}
-        type={inputType === 'number' ? 'text' : inputType} // 숫자 입력도 text로 처리하고 유효성 검사함
+        type={inputType === 'number' ? 'text' : inputType}
       />
     </StyledInfoInput>
   );
-};
-
-InfoInput.propTypes = {
-  text: PropTypes.string,
-  origValue: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
-    .isRequired,
-  editValue: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  width: PropTypes.string,
-  padding: PropTypes.string.isRequired,
-  align: PropTypes.string.isRequired,
-  edit: PropTypes.bool,
-  inputType: PropTypes.oneOf(['text', 'number', 'no-korean', 'eng-num']),
 };
 
 export default InfoInput;
