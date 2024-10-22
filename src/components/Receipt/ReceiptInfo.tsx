c4a1447843909bf3422f38c41ae2c649397a128d
JIN921 <wlslwlsl808@gmail.com>
2024-10-22 18:31:56 +0900

    [refactor] 영수증 컴포넌트 스타일 추가 분리

--- src/components/Receipt/ReceiptInfo.tsx
+++ src/components/Receipt/ReceiptInfo.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
 import Caption from '@/components/Button/Caption';
 import theme from '@/styles/theme';
 import React from 'react';
+import * as S from '@/styles/receipt/ReceiptInfo.styled';
 
 interface ReceiptInfoProps {
   money: string;
@@ -9,65 +9,23 @@ interface ReceiptInfoProps {
   memo: string;
 }
 
-const MemberWrapper = styled.div`
-  width; 88%;
-  margin: 16px 6% 0 6%;
-  font-family: ${theme.font.family.pretendard_regular};
-`;
-
-const StyledReceiptBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const StyledCaptionBox = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const StyledTextBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 10px;
-  margin-top: -2px;
-`;
-
-const StyledMemoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  margin-left: 10px;
-`;
-
-const Text = styled.div`
-  font-size: 16px;
-`;
-
-const SmallText = styled.div`
-  margin-top: 7px;
-  font-size: 12px;
-  color: rgba(255, 255, 255, 0.5);
-`;
-
 const ReceiptInfo: React.FC<ReceiptInfoProps> = ({ money, date, memo }) => {
   return (
-    <MemberWrapper>
-      <StyledReceiptBox>
-        <StyledCaptionBox>
+    <S.MemberWrapper>
+      <S.StyledReceiptBox>
+        <S.StyledCaptionBox>
           <Caption color={theme.color.main.negative}>지출</Caption>
-          <StyledTextBox>
-            <Text>{memo}</Text>
-            <SmallText>{date}</SmallText>
-          </StyledTextBox>
-        </StyledCaptionBox>
-        <StyledMemoBox>
-          <Text>{money}원</Text>
-          <SmallText>&nbsp;</SmallText>
-        </StyledMemoBox>
-      </StyledReceiptBox>
-    </MemberWrapper>
+          <S.StyledTextBox>
+            <S.Text>{memo}</S.Text>
+            <S.SmallText>{date}</S.SmallText>
+          </S.StyledTextBox>
+        </S.StyledCaptionBox>
+        <S.StyledMemoBox>
+          <S.Text>{money}원</S.Text>
+          <S.SmallText>&nbsp;</S.SmallText>
+        </S.StyledMemoBox>
+      </S.StyledReceiptBox>
+    </S.MemberWrapper>
   );
 };
 
