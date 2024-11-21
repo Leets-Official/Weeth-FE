32eb712571e09976175c32f279c3d7fa6acfc060
JIN921 <wlslwlsl808@gmail.com>
2024-11-21 14:05:23 +0900

    feat: PostList에 PostItem 매핑

--- src/components/Board/PostList.tsx
+++ src/components/Board/PostList.tsx
@@ -1,5 +1,7 @@
 import theme from '@/styles/theme';
 import styled from 'styled-components';
+import mockBoard from '../mockData/mockBoard';
+import PostListItem from './PostListItem';
 
 const Container = styled.div`
   margin: 5px 25px 0 25px;
@@ -10,7 +12,22 @@ const Line = styled.div`
 `;
 
 const PostList = () => {
-  return <Container />;
+  return (
+    <Container>
+      {mockBoard.map((post, index) => (
+        <div key={index}>
+          <PostListItem
+            name={post.name}
+            time={post.time}
+            title={post.title}
+            content={post.content}
+            totalComments={post.totalComments}
+          />
+          <Line />
+        </div>
+      ))}
+    </Container>
+  );
 };
 
 export default PostList;
