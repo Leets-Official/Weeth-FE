1f88b282e9f604c6f995413cb9257349f379cfc9
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-11-22 04:31:16 +0900

    refactor: 캘린더 모달 스타일 이동

--- src/components/Calendar/CalendarHeader.tsx
+++ src/components/Calendar/CalendarHeader.tsx
@@ -1,12 +1,30 @@
 import { UserContext } from '@/api/UserContext';
 import ModalMonthContent from '@/components/Calendar/ModalMonthContent';
-import { monthModalStyles } from '@/styles/calendar/CalendarHeader.styled';
 import React, { useContext, useState } from 'react';
 import Modal from 'react-modal';
 
 import Header from '../Header/Header';
 
 Modal.setAppElement('#root');
+
+const monthModalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    backdropFilter: 'blur(5px)', // 표준 CSS 속성
+    WebkitBackdropFilter: 'blur(5px)', // -webkit- 접두사를 사용한 속성
+    zIndex: 1000,
+    width: '100%',
+    height: '100%',
+    top: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: '55px auto', // 블러가 시작되는 위치를 정하고
+  },
+  content: {
+    margin: '10px auto', // 모달은 블러 시작점으로부터 10px 떨어진 곳에 위치
+  },
+};
 
 interface CalendarHeaderProps {
   month: number;
