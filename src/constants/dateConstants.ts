a574b6201ab7c999d5cbfe78e2bba7914a3d73e4
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-11-21 22:19:30 +0900

    feat: 현재 시간 iso string 상수화

--- src/constants/dateConstants.ts
+++ src/constants/dateConstants.ts
@@ -5,3 +5,5 @@ export const CURRENT_DAY = new Date().getDate();
 export const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export const WEEK_DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+
+export const CURRENT_ISO_STRING = new Date().toISOString();
