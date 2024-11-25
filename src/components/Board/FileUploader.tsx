309303525e4ce7ed1a74fb6f5fe967660f7d3ed7
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-11-26 04:49:42 +0900

    feat: 파일 업로드

--- src/components/Board/FileUploader.tsx
+++ src/components/Board/FileUploader.tsx
@@ -1,4 +1,5 @@
 import icClip from '@/assets/images/ic_clip.svg';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 export const FileButton = styled.img`
@@ -8,7 +9,43 @@ export const FileButton = styled.img`
 `;
 
 const FileUploader = () => {
-  return <FileButton src={icClip} />;
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      setFiles((prevFiles) => [...prevFiles, ...Array.from(selectedFiles)]);
+    }
+  };
+
+  return (
+    <>
+      {files.length > 0 && (
+        <ul>
+          {files.map((file) => (
+            <li key={file.name}>
+              {file.name} ({file.size} bytes)
+            </li>
+          ))}
+        </ul>
+      )}
+      <FileButton src={icClip} onClick={handleClick} />
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+        multiple
+      />
+    </>
+  );
 };
 
 export default FileUploader;
--- src/pages/BoardPost.tsx
+++ src/pages/BoardPost.tsx
@@ -2,20 +2,34 @@ import PostEditor from '@/components/Board/PostEditor';
 import Header from '@/components/Header/Header';
 import * as S from '@/styles/board/BoardPost.styled';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // TODO: 글쓰기 타입에 따라 어드민 체크
 const BoardPost = () => {
+  const navi = useNavigate();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
   const isTitleEmpty = title.trim() === '';
   const isContentEmpty = content.trim() === '';
 
+  const onSave = () => {
+    if (!isTitleEmpty && !isContentEmpty) {
+      // api 호출 로직
+      navi('/board');
+    } else if (isTitleEmpty) {
+      // TODO: toast로 변경 예정
+      alert('제목을 입력해주세요.');
+    } else if (isContentEmpty) {
+      alert('내용을 입력해주세요.');
+    }
+  };
+
   return (
     <S.PostWrapper>
       <Header
         title="글 쓰기"
-        onClickRightButton={() => {}}
+        onClickRightButton={onSave}
         RightButtonType="TEXT"
         isComplete={!isTitleEmpty && !isContentEmpty}
       />
