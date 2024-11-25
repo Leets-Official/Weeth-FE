5155afdf8a500488f896a857463aa08d1773ed58
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-11-26 04:03:13 +0900

    feat: input 값 저장 및 입력상태 체크

--- src/components/Board/PostEditor.tsx
+++ src/components/Board/PostEditor.tsx
@@ -3,13 +3,31 @@ import * as S from '@/styles/board/BoardPost.styled';
 import FileUploader from './FileUploader';
 
 // TODO: 글쓰기 타입에 따라 어드민 체크
-const PostEditor = () => {
+const PostEditor = ({
+  title,
+  setTitle,
+  content,
+  setContent,
+}: {
+  title: string;
+  setTitle: (value: string) => void;
+  content: string;
+  setContent: (value: string) => void;
+}) => {
   return (
     <>
       <S.PostWrapper>
-        <S.TitleInput placeholder="제목" />
+        <S.TitleInput
+          placeholder="제목"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <Line />
-        <S.ContentInput placeholder="내용을 입력하세요." />
+        <S.ContentInput
+          placeholder="내용을 입력하세요."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
       </S.PostWrapper>
       <FileUploader />
     </>
--- src/pages/BoardPost.tsx
+++ src/pages/BoardPost.tsx
@@ -1,17 +1,30 @@
 import PostEditor from '@/components/Board/PostEditor';
 import Header from '@/components/Header/Header';
 import * as S from '@/styles/board/BoardPost.styled';
+import { useState } from 'react';
 
 // TODO: 글쓰기 타입에 따라 어드민 체크
 const BoardPost = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const isTitleEmpty = title.trim() === '';
+  const isContentEmpty = content.trim() === '';
+
   return (
     <S.PostWrapper>
       <Header
         title="글 쓰기"
         onClickRightButton={() => {}}
         RightButtonType="TEXT"
+        isComplete={!isTitleEmpty && !isContentEmpty}
       />
-      <PostEditor />
+      <PostEditor
+        title={title}
+        setTitle={setTitle}
+        content={content}
+        setContent={setContent}
+      />
     </S.PostWrapper>
   );
 };
