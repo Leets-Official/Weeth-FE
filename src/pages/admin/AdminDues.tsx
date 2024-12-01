e7f3895d1955375ffb90fcdc1854a6a363f219fb
김지원 <gimjiwon@MacBook-Air.local>
2024-12-01 19:42:53 +0900

    chore: admin 폴더 기초 세팅

---
+++ src/pages/admin/AdminDues.tsx
@@ -0,0 +1,4 @@
+const AdminDues: React.FC = () => {
+  return <div>회비</div>;
+};
+export default AdminDues;
---
+++ src/pages/admin/components/NavMenu.tsx
@@ -0,0 +1,4 @@
+const NavMenu: React.FC = () => {
+  return <div>navmenu</div>;
+};
+export default NavMenu;
