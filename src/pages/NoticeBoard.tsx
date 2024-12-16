22c54c3f5d09bfd129915419057aaf08297dd93b
JIN921 <wlslwlsl808@gmail.com>
2024-12-17 01:17:34 +0900

    feat: 공지 게시판 페이지에도 적용

--- src/pages/NoticeBoard.tsx
+++ src/pages/NoticeBoard.tsx
@@ -1,8 +1,12 @@
-import styled from 'styled-components';
-import PostList from '@/components/Board/PostList';
+import { useEffect, useRef, useState } from 'react';
 import Info from '@/components/Board/Info';
+import styled from 'styled-components';
+import PostListItem from '@/components/Board/PostListItem';
 import NoticeHeader from '@/components/Board/NoticeHeader';
 import EditDelModal from '@/components/Modal/EditDelModal';
+import formatDate from '@/hooks/formatDate';
+import theme from '@/styles/theme';
+import useGetBoardInfo from '@/api/useGetBoardInfo';
 
 const Container = styled.div`
   display: flex;
@@ -12,13 +16,96 @@ const Container = styled.div`
   margin-bottom: 50px;
 `;
 
+const Line = styled.div`
+  border: 1px solid;
+  color: ${(props) => props.theme.color.gray[30]};
+  margin-top: 10px;
+`;
+
+const PostList = styled.div`
+  margin: 5px 25px 0 25px;
+`;
+
+const Text = styled.div`
+  text-align: center;
+  margin: 10px;
+  font-family: ${theme.font.semiBold};
+`;
+
+interface Content {
+  id: number;
+  name: string;
+  title: string;
+  content: string;
+  time: string;
+  commentCount: number;
+}
+
 const NoticeBoard = () => {
-  const isAdmin = true;
+  // TODO: 어드민인지 확인해서 true false 변경해주기
+  const isPostBtn = true;
+
+  const [posts, setPosts] = useState<Content[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const BASE_URL = import.meta.env.VITE_API_URL;
+  const path = 'notices';
+
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  // Intersection Observer 설정
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0];
+        if (firstEntry.isIntersecting && hasMore && !isLoading) {
+          useGetBoardInfo(
+            BASE_URL,
+            path,
+            pageNumber,
+            setPosts,
+            setHasMore,
+            setIsLoading,
+          );
+          setPageNumber((prevPage) => prevPage + 1);
+        }
+      },
+      { root: null, rootMargin: '0px', threshold: 0.1 },
+    );
+
+    if (observerRef.current) observer.observe(observerRef.current);
+
+    return () => {
+      if (observerRef.current) observer.unobserve(observerRef.current);
+    };
+  }, [hasMore, isLoading, pageNumber]);
+
   return (
     <Container>
       <NoticeHeader showModal={false} ModalComponent={EditDelModal} />
-      <Info title="공지 게시판" isEditButtonVisible={isAdmin} />
-      <PostList board="notices" />
+      <Info title="공지 게시판" isEditButtonVisible={isPostBtn} />
+      {posts.map((post) => (
+        <PostList key={post.id}>
+          <PostListItem
+            name={post.name}
+            time={formatDate(post.time)}
+            title={post.title}
+            content={post.content}
+            totalComments={post.commentCount}
+          />
+          <Line />
+        </PostList>
+      ))}
+      {hasMore && (
+        <div
+          ref={observerRef}
+          style={{ height: '20px', backgroundColor: 'transparent' }}
+        />
+      )}
+      {isLoading && <Text>로딩 중...</Text>}
+      {!hasMore && <Text>마지막 게시물입니다.</Text>}
     </Container>
   );
 };
