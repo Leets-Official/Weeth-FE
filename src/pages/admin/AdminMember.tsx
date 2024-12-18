de185f56116889fa637ed0459455780ab39c1401
dahhyeon <dahhyeon22@gmail.com>
2024-12-18 15:37:10 +0900

    feat:페이지 전체 배경색 및 너비 설정을 위한 PageWrapper 추가

--- src/pages/admin/AdminMember.tsx
+++ src/pages/admin/AdminMember.tsx
@@ -1,15 +1,33 @@
 import SearchBar from '@/components/Admin/SearchBar';
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const PageWrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f2f9f8;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+`;
 
 const AdminMember: React.FC = () => {
   return (
-    <Container>
-      <SearchBar />
-      <div>멤버</div>
-    </Container>
+    <PageWrapper>
+      <Container>
+        <SearchBar />
+        <div>멤버</div>
+      </Container>
+    </PageWrapper>
   );
 };
+
 export default AdminMember;
 
