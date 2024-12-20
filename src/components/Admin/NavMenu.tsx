267332bd0b5a783204911dfeb8b526bdbd026459
dahhyeon <dahhyeon22@gmail.com>
2024-12-20 20:17:20 +0900

    refactor:로고 및 제목을 컴포넌트로 분리

--- src/components/Admin/NavMenu.tsx
+++ src/components/Admin/NavMenu.tsx
@@ -1,25 +1,17 @@
 import styled from 'styled-components';
-import logo from '@/assets/images/logo/logo_initial_Xmas.svg';
+import NavHeader from './NavHeader';
 
 const Sidebar = styled.div`
   width: 248px;
-  display: flex;
   background-color: #ffffff;
   border: 1px solid #f2f2f2;
-  justify-content: center;
-  align-items: center;
   box-shadow: 0px 3px 8px 0px rgba(133, 141, 138, 0.2);
-`;
-
-const StyledTitle = styled.div`
-  font-weight: 500;
 `;
 
 const NavMenu: React.FC = () => {
   return (
     <Sidebar>
-      <img src={logo} alt="weeth로고" />
-      <StyledTitle>WEETH ADMIN</StyledTitle>
+      <NavHeader />
     </Sidebar>
   );
 };
