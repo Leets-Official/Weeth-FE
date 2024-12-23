1f61dd3ded8e90c2079af4ce4c7c9d89498dec61
김지원 <jiwonkim0531@gachon.ac.kr>
2024-12-23 16:47:53 +0900

    feat: styles 분리

--- src/components/Admin/AttendDropdown.tsx
+++ src/components/Admin/AttendDropdown.tsx
@@ -1,201 +1,32 @@
-import styled from 'styled-components';
 import Search from '@/assets/images/ic_admin_search.svg';
 import { useState } from 'react';
-import theme from '@/styles/theme';
+import {
+  Wrapper,
+  SearchWrapper,
+  SearchBar,
+  EditButton,
+  Edit,
+  ButtonWrapper,
+  CancelButton,
+  SaveButton,
+  UserWrapper,
+  Info,
+  InfoWrapper,
+  InfoBox,
+  User,
+  Attend,
+  MemberWrapper,
+  Member,
+  UserName,
+  UserInfo,
+  Check,
+  CheckGap,
+  StatusBox,
+  StatusWarpper,
+} from '@/styles/admin/AttendDropdown.styled';
 
 import CheckBox from '@/assets/images/ic_admin_check.svg';
 import RadioButton from './RadioButton';
-
-const Wrapper = styled.div`
-  width: 815px;
-  backgroun-color: #ffffff;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #dedede;
-
-  &:last-child {
-    margin-bottom: 15px;
-  }
-`;
-
-const SearchWrapper = styled.div`
-  width: 784px;
-  height: 48px;
-  display: flex;
-  justify-content: space-between;
-  margin-top: 15px;
-`;
-
-const SearchBar = styled.div`
-  width: 492px;
-  height: 48px;
-  border: 1px solid #dedede;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  color: #4e4e4e;
-  font-family: ${theme.font.regular};
-  font-size: 18px;
-  padding-left: 10px;
-`;
-
-const EditButton = styled.div<{ isEdit: boolean }>``;
-
-const Edit = styled.div`
-  width: 64px;
-  height: 48px;
-  background-color: #2f2f2f;
-  color: white;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 8px;
-`;
-
-const CancelButton = styled.div`
-  width: 64px;
-  height: 48px;
-  background-color: #a6a6a6;
-  color: white;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const SaveButton = styled.div`
-  width: 64px;
-  height: 48px;
-  background-color: #2f2f2f;
-  color: white;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const UserWrapper = styled.div`
-  width: 784px;
-  background-color: #ffffff;
-  border: 1px solid #dedede;
-  margin-top: 15px;
-  margin-bottom: 15px;
-`;
-
-const Info = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const InfoWrapper = styled.div`
-  display: flex;
-  width: 157px;
-  height: 48px;
-  font-family: ${theme.font.semiBold};
-  font-size: 18px;
-`;
-
-const InfoBox = styled.div`
-  border: 1px solid #dedede;
-  width: 79px;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const User = styled.div`
-  width: 626px;
-  height: 48px;
-  border: 1px solid #dedede;
-  font-family: ${theme.font.semiBold};
-  font-size: 18px;
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-`;
-
-const Attend = styled.div`
-  width: 158px;
-  height: 48px;
-  border: 1px solid #dedede;
-  font-family: ${theme.font.semiBold};
-  font-size: 18px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const MemberWrapper = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-const Member = styled.div`
-  width: 626px;
-  height: 74px;
-  border: 1px solid #dedede;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 5px;
-  padding-left: 10px;
-`;
-
-const UserName = styled.div`
-  font-family: ${theme.font.semiBold};
-  font-size: 18px;
-`;
-
-const UserInfo = styled.div`
-  font-family: ${theme.font.regular};
-  font-size: 14px;
-`;
-
-const Check = styled.div`
-  width: 158px;
-  height: 74px;
-  border: 1px solid #dedede;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-`;
-
-const CheckGap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-`;
-
-const StatusWarpper = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-`;
-
-const StatusBox = styled.div`
-  flex: 1;
-  heigth: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-right: 2px solid #dedede;
-  &:last-child {
-    border-right: none;
-  }
-`;
 
 interface AttendDropdownItem {
   id: number;
--- src/components/Admin/Attendance.tsx
+++ src/components/Admin/Attendance.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import {
+  AttendanceTable,
+  Wrapper,
+  DateInfoWrapper,
+  DateText,
+  ContentText,
+  Button,
+} from '@/styles/admin/Attendance.styled';
 
-import theme from '@/styles/theme';
 import DropDown from '@/assets/images/ic_admin_cardinal.svg';
 import AttendDropdown from './AttendDropdown';
 
@@ -11,48 +17,6 @@ interface AttendanceItem {
   content: string;
   start: string;
 }
-
-const AttendanceTable = styled.div`
-  width: 815px;
-  background-color: #f2f9f8;
-  border-radius: 10px 10px 0px 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 15px;
-  border-bottom: 1px solid #dedede;
-
-  &:last-child {
-    margin-bottom: 15px;
-  }
-`;
-
-const Wrapper = styled.div`
-  width: 100%;
-  padding: 25px 25px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const DateInfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const DateText = styled.span`
-  font-family: ${theme.font.regular};
-  font-size: 20px;
-  color: black;
-  margin-right: 15px;
-`;
-
-const ContentText = styled.span`
-  font-family: ${theme.font.regular};
-  font-size: 20px;
-  color: black;
-`;
-
-const Button = styled.div``;
 
 const Attendance: React.FC = () => {
   const [data] = useState<AttendanceItem[]>([
