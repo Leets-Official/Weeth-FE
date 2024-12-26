59e7637d69ad6e0f2a18ab6a9f3f077b594f0dd9
dahhyeon <dahhyeon22@gmail.com>
2024-12-26 19:09:43 +0900

    feat:기수순/오름차순 버튼 추가

---
+++ src/components/Admin/SortButton.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import SortIcon from '@/assets/images/ic_admin_change.svg';
+
+const SortButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #ecf9f6;
+  gap: 10px;
+  padding: 7px;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+const SortButton: React.FC = () => {
+  const [isAscending, setIsAscending] = useState(false);
+  const onClickSort = () => {
+    setIsAscending(!isAscending);
+  };
+
+  return (
+    <SortButtonWrapper onClick={onClickSort}>
+      {isAscending ? '오름차순' : '기수 순'}
+      <img src={SortIcon} alt="정렬" />
+    </SortButtonWrapper>
+  );
+};
+
+export default SortButton;
--- src/components/Admin/StatusList.tsx
+++ src/components/Admin/StatusList.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import StatusIndicator from './StatusIndicator';
+import SortButton from './SortButton';
 
 const FilterWrapper = styled.div`
   display: flex;
   justify-content: flex-start;
   gap: 16px;
   margin-bottom: 20px;
+  font-size: 14px;
 `;
 
 const StatusList: React.FC = () => {
@@ -14,6 +16,7 @@ const StatusList: React.FC = () => {
       <StatusIndicator status="승인 완료" />
       <StatusIndicator status="대기 중" />
       <StatusIndicator status="추방" />
+      <SortButton />
     </FilterWrapper>
   );
 };
