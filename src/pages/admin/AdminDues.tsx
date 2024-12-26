e4208c1c6cb1567b323a7564bf477dc59d5fe28f
김지원 <jiwonkim0531@gachon.ac.kr>
2024-12-26 17:34:52 +0900

    feat: 어드민 회비 페이지 전체 크기 조정

--- src/pages/admin/AdminDues.tsx
+++ src/pages/admin/AdminDues.tsx
@@ -1,4 +1,37 @@
+import NavMenu from '@/components/Admin/NavMenu';
+import styled from 'styled-components';
+import TopBar from '@/components/Admin/TopBar';
+import Cardinal from '@/components/Member/Cardinal';
+import { PageWrapper, Container } from './AdminMember';
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  border: 1px solid #f2f2f2;
+  height: 100%;
+`;
+
+const DuesWrapper = styled.div`
+  width: 814px;
+  background-color: #ffffff;
+`;
+
 const AdminDues: React.FC = () => {
-  return <div>회비</div>;
+  return (
+    <PageWrapper>
+      <NavMenu />
+      <ContentWrapper>
+        <TopBar
+          title="회비 관리"
+          description="기수 시작시 이월된 회비와 해당 기수 회비를 종합해 회비를 등록해주시기 바랍니다. 회비 등록은 기수당 한 번만 가능합니다."
+        />
+        <Container>
+          <Cardinal />
+          <DuesWrapper>총 회비</DuesWrapper>
+        </Container>
+      </ContentWrapper>
+    </PageWrapper>
+  );
 };
 export default AdminDues;
