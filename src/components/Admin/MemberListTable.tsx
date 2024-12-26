e5bf78b709d6256cb11b15d1e12a1ee4c03c490e
dahhyeon <dahhyeon22@gmail.com>
2024-12-26 19:10:30 +0900

    chore:필요없는 css 속성 제거

--- src/components/Admin/MemberListTable.tsx
+++ src/components/Admin/MemberListTable.tsx
@@ -15,13 +15,7 @@ export interface MemberListTableProps {
 
 export const TableWrapper = styled.div`
   width: 100%;
-  /* height: max-content; */
-  /* background-color: #fff; */
   font-size: 18px;
-  /* margin-top: 30px; */
-  /* box-shadow: 0px 3px 8px 0px rgba(133, 141, 138, 0.2); */
-  /* border-radius: 4px; */
-  /* box-sizing: border-box; */
   border-collapse: collapse;
 `;
 
