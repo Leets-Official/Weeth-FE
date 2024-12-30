f28fd6561c7995d2b1486153782bf2476b7904af
김지원 <jiwonkim0531@gachon.ac.kr>
2024-12-30 20:37:58 +0900

    feat: 회비 수정, 삭제 공통 컴포넌트 UI

--- src/components/Admin/Button.tsx
+++ src/components/Admin/Button.tsx
@@ -1,4 +1,31 @@
-const Button: React.FC = () => {
-  return <div>버튼</div>;
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+interface ButtonProps {
+  color: string;
+  description: string;
+}
+
+const Wrapper = styled.div<{ color: string }>`
+  width: 64px;
+  height: 48px;
+  background-color: ${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Description = styled.div`
+  font-size: 18px;
+  font-family: ${theme.font.regular};
+  color: white;
+`;
+
+const Button: React.FC<ButtonProps> = ({ color, description }) => {
+  return (
+    <Wrapper color={color}>
+      <Description>{description}</Description>
+    </Wrapper>
+  );
 };
 export default Button;
