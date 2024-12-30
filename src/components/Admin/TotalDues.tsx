38e01597332fada12508765a47db9af03ac08376
김지원 <jiwonkim0531@gachon.ac.kr>
2024-12-30 20:39:12 +0900

    feat: 기수에 따른 제목 변경

---
+++ src/components/Admin/TotalDues.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+import {
+  TotalDuesWrapper,
+  TopDues,
+  Title,
+} from '@/styles/admin/TotalDues.styled';
+import Box from './Box';
+
+interface TotalDuesProps {
+  getDuesText: () => string;
+}
+
+const BoxWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+  justify-content: center;
+`;
+
+const CardinalWrapper = styled.div`
+  margin-left: 30px;
+`;
+
+const InsideDues = styled.div`
+  width: 766px;
+  height: 274px;
+  font-size: 24px;
+  font-family: ${theme.font.semiBold};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const TotalDues: React.FC<TotalDuesProps> = ({ getDuesText }) => {
+  return (
+    <TotalDuesWrapper>
+      <TopDues>
+        <Title>총 회비</Title>
+      </TopDues>
+      <InsideDues>
+        <CardinalWrapper>{getDuesText()}</CardinalWrapper>
+        <BoxWrapper>
+          <Box
+            title="원금"
+            description="0원"
+            last="0000.00.00. 00:00"
+            color="#00DDA8"
+          />
+          <Box
+            title="현재"
+            description="0원"
+            last="2024.10.24. 15:13"
+            color="#2f2f2f"
+          />
+          <Box
+            title="사용"
+            description="0원"
+            last="0000.00.00. 00:00"
+            color="#2f2f2f"
+          />
+        </BoxWrapper>
+      </InsideDues>
+    </TotalDuesWrapper>
+  );
+};
+export default TotalDues;
