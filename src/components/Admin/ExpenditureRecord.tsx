495e462e0e099a458385db9a67d6e4415f3c3873
김지원 <jiwonkim0531@gachon.ac.kr>
2024-12-30 20:36:26 +0900

    feat: 회비 지출 기록 상세 컴포넌트

---
+++ src/components/Admin/ExpenditureRecord.tsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+import Receipt from '@/assets/images/ic_admin_receipt.svg';
+import Button from './Button';
+
+interface ExpenditureRecordProps {
+  date: string;
+  title: string;
+  money: number;
+  master: string;
+}
+
+const data: ExpenditureRecordProps[] = [
+  {
+    date: '2024.10.24.토요일',
+    title: '파티룸 대여',
+    money: 19302,
+    master: '파티룸 주인',
+  },
+  {
+    date: '2024.10.25.일요일',
+    title: '음식 주문',
+    money: 35000,
+    master: '음식점 주인',
+  },
+];
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Wrapper = styled.div`
+  width: 734px;
+  background-color: #fff;
+  border: 1px solid #dedede;
+`;
+
+const DateWrapper = styled.div`
+  width: 734px;
+  height: 48px;
+  border-bottom: 1px solid #dedede;
+  display: flex;
+  justify-content: space-between;
+  font-family: ${theme.font.regular};
+  font-size: 18px;
+  align-items: center;
+`;
+
+const Date = styled.div`
+  margin-left: 15px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+`;
+
+const ExpenditureWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const ExpenditureTitle = styled.div`
+  font-family: ${theme.font.semiBold};
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Master = styled.div`
+  margin-top: 20px;
+`;
+const ExpenditureMaster = styled.div`
+  font-family: ${theme.font.regular};
+  font-size: 18px;
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ExpenditureRecord: React.FC<ExpenditureRecordProps> = () => {
+  return (
+    <Container>
+      {data.map((item) => (
+        <Wrapper>
+          <DateWrapper>
+            <Date>{item.date}</Date>
+            <ButtonWrapper>
+              <Button description="수정" color="#323232" />
+              <Button description="삭제" color="#ff5858" />
+            </ButtonWrapper>
+          </DateWrapper>
+          <ExpenditureWrapper>
+            <div>
+              <ExpenditureTitle>{item.title}</ExpenditureTitle>
+              <ExpenditureTitle>{item.money}원</ExpenditureTitle>
+            </div>
+            <ExpenditureMaster>
+              <Master>{item.master}</Master>
+
+              <img src={Receipt} alt="Receipt" />
+            </ExpenditureMaster>
+          </ExpenditureWrapper>
+        </Wrapper>
+      ))}
+    </Container>
+  );
+};
+
+export default ExpenditureRecord;
