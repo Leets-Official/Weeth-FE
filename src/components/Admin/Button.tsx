5bcd5b46977e66e78a317c8f29dde3653824ee1f
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-02 23:33:25 +0900

    fix: width props 추가

--- src/components/Admin/Button.tsx
+++ src/components/Admin/Button.tsx
@@ -4,15 +4,17 @@ import theme from '@/styles/theme';
 interface ButtonProps {
   color: string;
   description: string;
+  width: string;
 }
 
-const Wrapper = styled.div<{ color: string }>`
-  width: 64px;
+const Wrapper = styled.div<{ color: string; width: string }>`
+  width: ${(props) => props.width || '64px'};
   height: 48px;
   background-color: ${(props) => props.color};
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
 `;
 
 const Description = styled.div`
@@ -21,9 +23,9 @@ const Description = styled.div`
   color: white;
 `;
 
-const Button: React.FC<ButtonProps> = ({ color, description }) => {
+const Button: React.FC<ButtonProps> = ({ color, description, width }) => {
   return (
-    <Wrapper color={color}>
+    <Wrapper color={color} width={width}>
       <Description>{description}</Description>
     </Wrapper>
   );
