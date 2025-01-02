2f6e66d56bbaa9061f25619ea29c7d75c576cf68
dahhyeon <dahhyeon22@gmail.com>
2025-01-02 17:53:59 +0900

    refactor:memberprovider 추가 및 data 제거

--- src/pages/admin/AdminMember.tsx
+++ src/pages/admin/AdminMember.tsx
@@ -3,27 +3,12 @@ import MemberListTable from '@/components/Admin/MemberListTable';
 import NavMenu from '@/components/Admin/NavMenu';
 import SearchBar from '@/components/Admin/SearchBar';
 import TopBar from '@/components/Admin/TopBar';
-import Button from '@/components/Button/Button';
+import { MemberProvider } from '@/components/Admin/context/MemberContext';
 import {
   PageWrapper,
   ContentWrapper,
   Container,
 } from '@/styles/admin/AdminLayout.styled';
-
-type MemberData = {
-  status: '승인 완료' | '대기 중' | '추방';
-  name: string;
-  role: string;
-  major: string;
-  cardinal: string;
-  phone: string;
-  studentId: string;
-  position: string;
-  attendance: number;
-  absence: number;
-  penalty: number;
-  joinDate: string;
-};
 
 const columns = [
   { key: 'name', header: '이름' },
@@ -39,91 +24,24 @@ const columns = [
   { key: 'joinDate', header: '가입일' },
 ];
 
-const data: MemberData[] = [
-  {
-    status: '승인 완료',
-    name: '김위드니',
-    role: '프론트엔드',
-    major: '소프트웨어전공',
-    cardinal: '4.3.2.1',
-    phone: '01000009999',
-    studentId: '202036123',
-    position: '사용자',
-    attendance: 12,
-    absence: 0,
-    penalty: 12,
-    joinDate: '2024.08.27',
-  },
-  {
-    status: '대기 중',
-    name: '박위드니',
-    role: '프론트엔드',
-    major: '미디어커뮤니케이션학과',
-    cardinal: '4',
-    phone: '01000009999',
-    studentId: '202036123',
-    position: '사용자',
-    attendance: 12,
-    absence: 0,
-    penalty: 12,
-    joinDate: '2024.08.27',
-  },
-  {
-    status: '승인 완료',
-    name: '최위드니',
-    role: '백엔드',
-    major: '컴퓨터공학과',
-    cardinal: '4.3',
-    phone: '01000009999',
-    studentId: '202036123',
-    position: '사용자',
-    attendance: 12,
-    absence: 0,
-    penalty: 12,
-    joinDate: '2024.08.27',
-  },
-  {
-    status: '추방',
-    name: '최위드니',
-    role: '백엔드',
-    major: '컴퓨터공학과',
-    cardinal: '4.3',
-    phone: '01000009999',
-    studentId: '202036123',
-    position: '사용자',
-    attendance: 12,
-    absence: 0,
-    penalty: 12,
-    joinDate: '2024.08.27',
-  },
-];
-
 const AdminMember: React.FC = () => {
   return (
-    <PageWrapper>
-      <NavMenu />
-      <ContentWrapper>
-        <TopBar
-          title="멤버 관리"
-          description="가입 승인 등 멤버를 관리하는 페이지입니다. 정기모임을 모두 입력하신 후에 가입 승인을 해주시길 바랍니다."
-        />
-        <Container>
-          <SearchBar />
-          <CardinalInfo />
-          <MemberListTable columns={columns} data={data} />
-          <Button
-            color="#fff"
-            textcolor="#000"
-            onClick={() => console.log('clicked')}
-            height="48px"
-            width="99px"
-            borderRadius="4px"
-          >
-            가입 승인
-          </Button>
-        </Container>
-      </ContentWrapper>
-    </PageWrapper>
+    <MemberProvider>
+      <PageWrapper>
+        <NavMenu />
+        <ContentWrapper>
+          <TopBar
+            title="멤버 관리"
+            description="가입 승인 등 멤버를 관리하는 페이지입니다. 정기모임을 모두 입력하신 후에 가입 승인을 해주시길 바랍니다."
+          />
+          <Container>
+            <SearchBar />
+            <CardinalInfo />
+            <MemberListTable columns={columns} data={data} />
+          </Container>
+        </ContentWrapper>
+      </PageWrapper>
+    </MemberProvider>
   );
 };
 
