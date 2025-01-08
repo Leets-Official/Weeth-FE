75a75a5e5bd55c18fda3e090fd070007f081a885
dahhyeon <dahhyeon22@gmail.com>
2025-01-08 21:35:30 +0900

    feat: 모달 기본 ui

--- src/components/Admin/MemberListTableRow.tsx
+++ src/components/Admin/MemberListTableRow.tsx
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import CheckBox from '@/assets/images/ic_admin_checkbox.svg';
 import UnCheckBox from '@/assets/images/ic_admin_uncheckbox.svg';
 import MeatBallSvg from '@/assets/images/ic_admin_meatball.svg';
+import { useState } from 'react';
 import { statusColors } from './StatusIndicator';
 import { MemberData, useMemberContext } from './context/MemberContext';
 import { Column } from './MemberListTable';
+import MemberDetailModal from './Modal/MemberDetailModal';
 
 const Row = styled.tr`
   border-bottom: 1px solid #dedede;
@@ -34,7 +36,7 @@ interface TableRowProps {
 
 const MemberListTableRow: React.FC<TableRowProps> = ({ data, columns }) => {
   const { selectedMembers, setSelectedMembers } = useMemberContext();
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const isChecked = selectedMembers.includes(data.studentId);
 
   const onClickToCheckBox = () => {
@@ -45,25 +47,32 @@ const MemberListTableRow: React.FC<TableRowProps> = ({ data, columns }) => {
     );
   };
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-    <Row>
-      <StatusCell statusColor={statusColors[data.status]} />
+    <>
+      <Row>
+        <StatusCell statusColor={statusColors[data.status]} />
 
-      <SvgWrapper onClick={onClickToCheckBox}>
-        <img
-          src={isChecked ? CheckBox : UnCheckBox}
-          alt={isChecked ? 'checked' : 'unchecked'}
-        />
-      </SvgWrapper>
-      {columns.map((column) => (
-        <Cell key={column.key}>{data[column.key]}</Cell>
-      ))}
+        <SvgWrapper onClick={onClickToCheckBox}>
+          <img
+            src={isChecked ? CheckBox : UnCheckBox}
+            alt={isChecked ? 'checked' : 'unchecked'}
+          />
+        </SvgWrapper>
+        {columns.map((column) => (
+          <Cell key={column.key}>{data[column.key]}</Cell>
+        ))}
 
-      {/* 추후 클릭 이벤트 추가 */}
-      <SvgWrapper>
-        <img src={MeatBallSvg} alt="미트볼 메뉴" />
-      </SvgWrapper>
-    </Row>
+        {/* 추후 클릭 이벤트 추가 */}
+        <SvgWrapper onClick={openModal}>
+          <img src={MeatBallSvg} alt="미트볼 메뉴" />
+        </SvgWrapper>
+      </Row>
+
+      {isModalOpen && <MemberDetailModal data={data} onClose={closeModal} />}
+    </>
   );
 };
 
---
+++ src/components/Admin/Modal/MemberDetailModal.tsx
@@ -0,0 +1,73 @@
+import { MemberData } from '@/components/Admin/context/MemberContext';
+import theme from '@/styles/theme';
+import Modal from 'react-modal';
+import { styled } from 'styled-components';
+
+interface MemberDetailModalProps {
+  data: MemberData;
+  onClose: () => void;
+}
+
+const ModalContent = styled.div`
+  background-color: white;
+  border-radius: 4px;
+  width: 400px;
+  box-shadow: 0px 3px 8px 0px rgba(133, 141, 138, 0.2);
+  color: #000;
+`;
+
+const Title = styled.div`
+  background-color: #f2f9f8;
+  font-family: ${theme.font.regular};
+  width: 100%;
+  height: 96px;
+  color: #000;
+  font-weight: 700;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+`;
+const ModalContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+`;
+const MemberDetailModal: React.FC<MemberDetailModalProps> = ({
+  data,
+  onClose,
+}) => {
+  return (
+    <Modal
+      isOpen={true}
+      onRequestClose={onClose}
+      style={{
+        overlay: {
+          backgroundColor: 'rgba(0,0,0,0.5)',
+        },
+        content: {
+          top: '35%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+          borderRadius: '8px',
+          width: '50%',
+          height: 'auto',
+          maxWidth: '830px',
+          maxHeight: '90vh',
+          padding: 0,
+          overflow: 'hidden',
+        },
+      }}
+    >
+      <Title>멤버 관리 버튼 </Title>
+      <ModalContentWrapper>
+        <ModalContent>회원정보</ModalContent>
+        <ModalContent>활동정보</ModalContent>
+      </ModalContentWrapper>
+    </Modal>
+  );
+};
+
+export default MemberDetailModal;
