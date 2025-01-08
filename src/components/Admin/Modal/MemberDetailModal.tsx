ee83709e55302fe6fdc1314fa32a76d780e7b0be
dahhyeon <dahhyeon22@gmail.com>
2025-01-09 08:19:17 +0900

    style:모달 컨텐츠 위치 및 정렬 수정

--- src/components/Admin/Modal/MemberDetailModal.tsx
+++ src/components/Admin/Modal/MemberDetailModal.tsx
@@ -7,6 +7,7 @@ import closeIcon from '@/assets/images/ic_admin_close.svg';
 import ButtonGroup from '../ButtonGroup';
 import StatusIndicator from '../StatusIndicator';
 
+// pr: react-modal 사용
 interface MemberDetailModalProps {
   data: MemberData;
   onClose: () => void;
@@ -25,9 +26,10 @@ const ModalContent = styled.div`
   box-shadow: 0px 3px 8px 0px rgba(133, 141, 138, 0.2);
   color: #000;
   display: flex;
+  flex: 1.5;
   flex-direction: column;
   gap: 20px;
-  padding: 15px;
+  padding: 15px 15px 50px 15px;
 `;
 const Title = styled.div`
   font-family: ${theme.font.regular};
@@ -48,16 +50,25 @@ const TitleContainer = styled.div`
 const FontStyle = styled.div<FontStyleProps>`
   font-size: ${({ fontSize }) => fontSize || '18px'};
   font-weight: ${({ fontWeight }) => fontWeight || '500'};
-  color: ${({ color }) => color || '#000'};
+  color: ${({ color }) => color};
 `;
 
 const ModalContentWrapper = styled.div`
   font-family: ${theme.font.regular};
   display: flex;
+  flex: 0;
+  height: calc(100% - 96px - 96px);
   justify-content: center;
   align-items: center;
   gap: 20px;
   padding: 20px;
+`;
+
+const ModalContainer = styled.div`
+  position: relative;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ModalFooter = styled.div`
@@ -67,19 +78,47 @@ const ModalFooter = styled.div`
   display: flex;
   align-items: center;
   padding-left: 20px;
+  position: absolute;
+  bottom: 0;
 `;
 
 const Flex = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  gap: 30px;
+  gap: 20px;
 `;
+
+const LabelFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  color: #a6a6a6;
+`;
+
+const DataFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  color: #000;
+`;
+
 const CloseIcon = styled.img`
   cursor: pointer;
   margin-right: 20px;
 `;
 
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 40px;
+`;
+
+const ActivityContent = styled(ModalContent)`
+  flex: 1;
+`;
 const getHighestCardinal = (cardinal: string): string => {
   const highestCardinal = cardinal.split('.')[0];
   return `${highestCardinal}기`;
@@ -139,76 +178,68 @@ const MemberDetailModal: React.FC<MemberDetailModalProps> = ({
         },
       }}
     >
-      <TitleContainer>
-        <Title>멤버 관리 버튼 </Title>
-        <CloseIcon src={closeIcon} alt="close" onClick={onClose} />
-      </TitleContainer>
-      <ModalContentWrapper>
-        <ModalContent>
-          <FontStyle fontSize="12px">회원정보</FontStyle>
-          <Flex>
-            <FontStyle fontSize="24px" fontWeight="700">
-              {data.name} &nbsp;
-              {getHighestCardinal(data.cardinal)}
-            </FontStyle>
-            <StatusIndicator status={data.status} />
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">직급</FontStyle>
-            <FontStyle>{data.position}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">역할</FontStyle>
-            <FontStyle>{data.role}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">학과</FontStyle>
-            <FontStyle>{data.major}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">전화번호</FontStyle>
-            <FontStyle>{data.phone}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">학번</FontStyle>
-            <FontStyle>{data.studentId}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">이메일</FontStyle>
-            <FontStyle>{data.email}</FontStyle>
-          </Flex>
-        </ModalContent>
-        <ModalContent>
-          <FontStyle fontSize="12px">활동정보</FontStyle>
-          <Flex>
-            <FontStyle color="#a6a6a6">활동기수</FontStyle>
-            <FontStyle>{data.cardinal}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">상태</FontStyle>
-            <FontStyle>{data.membershipType}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">가입일</FontStyle>
-            <FontStyle>{data.joinDate}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">출석</FontStyle>
-            <FontStyle>{data.attendance}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">결석</FontStyle>
-            <FontStyle>{data.absence}</FontStyle>
-          </Flex>
-          <Flex>
-            <FontStyle color="#a6a6a6">패널티</FontStyle>
-            <FontStyle color="#ff5858">{data.penalty}</FontStyle>
-          </Flex>
-        </ModalContent>
-      </ModalContentWrapper>
-      <ModalFooter>
-        <ButtonGroup buttons={buttons} hasEndGap />
-      </ModalFooter>
+      <ModalContainer>
+        <TitleContainer>
+          <Title>멤버 관리 버튼 </Title>
+          <CloseIcon src={closeIcon} alt="close" onClick={onClose} />
+        </TitleContainer>
+        <ModalContentWrapper>
+          <ModalContent>
+            <FontStyle fontSize="12px">회원정보</FontStyle>
+            <Flex>
+              <FontStyle fontSize="24px" fontWeight="700">
+                {data.name} &nbsp;
+                {getHighestCardinal(data.cardinal)}
+              </FontStyle>
+              <StatusIndicator status={data.status} />
+            </Flex>
+            <FlexWrapper>
+              <LabelFlex>
+                <FontStyle>직급</FontStyle>
+                <FontStyle>역할</FontStyle>
+                <FontStyle>학과</FontStyle>
+                <FontStyle>전화번호</FontStyle>
+                <FontStyle>학번</FontStyle>
+                <FontStyle>이메일</FontStyle>
+              </LabelFlex>
+
+              <DataFlex>
+                <FontStyle>{data.position}</FontStyle>
+                <FontStyle>{data.role}</FontStyle>
+                <FontStyle>{data.major}</FontStyle>
+                <FontStyle>{data.phone}</FontStyle>
+                <FontStyle>{data.studentId}</FontStyle>
+                <FontStyle>{data.email}</FontStyle>
+              </DataFlex>
+            </FlexWrapper>
+          </ModalContent>
+          <ActivityContent>
+            <FontStyle fontSize="12px">활동정보</FontStyle>
+            <FlexWrapper>
+              <LabelFlex>
+                <FontStyle>활동기수</FontStyle>
+                <FontStyle>상태</FontStyle>
+                <FontStyle>가입일</FontStyle>
+                <FontStyle>출석</FontStyle>
+                <FontStyle>결석</FontStyle>
+                <FontStyle>패널티</FontStyle>
+              </LabelFlex>
+
+              <DataFlex>
+                <FontStyle>{data.cardinal}</FontStyle>
+                <FontStyle>{data.membershipType}</FontStyle>
+                <FontStyle>{data.joinDate}</FontStyle>
+                <FontStyle>{data.attendance}</FontStyle>
+                <FontStyle>{data.absence}</FontStyle>
+                <FontStyle color="#ff5858">{data.penalty}</FontStyle>
+              </DataFlex>
+            </FlexWrapper>
+          </ActivityContent>
+        </ModalContentWrapper>
+        <ModalFooter>
+          <ButtonGroup buttons={buttons} hasEndGap />
+        </ModalFooter>
+      </ModalContainer>
     </Modal>
   );
 };
