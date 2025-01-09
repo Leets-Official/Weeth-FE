9f80f46b8336a0db5fa5f4b6a81c020b796c9931
dahhyeon <dahhyeon22@gmail.com>
2025-01-09 17:54:58 +0900

    feat:기본 페이지 ui 추가

--- src/pages/admin/AdminPenalty.tsx
+++ src/pages/admin/AdminPenalty.tsx
@@ -1,4 +1,23 @@
+import NavMenu from '@/components/Admin/NavMenu';
+import TopBar from '@/components/Admin/TopBar';
+import {
+  Container,
+  PageWrapper,
+  ContentWrapper,
+} from '@/styles/admin/AdminLayout.styled';
+
 const AdminPenalty: React.FC = () => {
-  return <div>패널티</div>;
+  return (
+    <PageWrapper>
+      <NavMenu />
+      <ContentWrapper>
+        <TopBar
+          title="패널티 관리"
+          description="기수를 선택하고, 해당 멤버에 대한 패널티를 수정하는 페이지입니다."
+        />
+        <Container>컨테이너</Container>
+      </ContentWrapper>
+    </PageWrapper>
+  );
 };
 export default AdminPenalty;
