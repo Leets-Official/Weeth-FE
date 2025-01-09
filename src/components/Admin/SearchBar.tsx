c4cd72d35e4cbd36a4fadb0d97377233cb648a2e
dahhyeon <dahhyeon22@gmail.com>
2025-01-09 18:30:09 +0900

    style:검색바 높이 수정

--- src/components/Admin/SearchBar.tsx
+++ src/components/Admin/SearchBar.tsx
@@ -12,10 +12,12 @@ export const SearchBarWrapper = styled.div`
   border-radius: 4px;
   margin-top: 30px;
   box-shadow: 0px 3px 8px 0px rgba(133, 141, 138, 0.2);
+  gap: 15px;
 `;
 
 export const StyledInput = styled.input`
   width: 100%;
+  height: 48px;
   box-sizing: border-box;
   padding: 12px 12px 12px 40px;
   border: 1px solid ${theme.color.gray[65]};
@@ -48,4 +50,5 @@ const SearchBar: React.FC<SearchBarProps> = ({ isWrapped = true }) => {
 
   return isWrapped ? <SearchBarWrapper>{content}</SearchBarWrapper> : content;
 };
+
 export default SearchBar;
