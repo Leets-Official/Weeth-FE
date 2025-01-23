1a5f5ceeeae12622341c6684d63eb8535d4d7a5d
dahhyeon <dahhyeon22@gmail.com>
2025-01-24 00:40:12 +0900

    chore:정렬 구현 코드 제거

--- src/components/Admin/SortButton.tsx
+++ src/components/Admin/SortButton.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import SortIcon from '@/assets/images/ic_admin_change.svg';
-import { useMemberContext } from './context/MemberContext';
 
 const SortButtonWrapper = styled.div`
   display: flex;
@@ -13,39 +12,11 @@ const SortButtonWrapper = styled.div`
   cursor: pointer;
 `;
 const SortButton: React.FC = () => {
-  const { members, setFilteredMembers } = useMemberContext();
   const [isAscending, setIsAscending] = useState(false);
 
   const onClickSort = () => {
     setIsAscending(!isAscending);
-
-    const sortedMembers = [...members].sort((a, b) => {
-      // 이름 오름차순
-      if (isAscending) {
-        return a.name.localeCompare(b.name);
-      }
-      // 기수 내림차순
-      const cardinalA = a.cardinal.split('.').map(Number);
-      const cardinalB = b.cardinal.split('.').map(Number);
-
-      for (
-        let i = 0;
-        i < Math.max(cardinalA.length, cardinalB.length);
-        i += 1
-      ) {
-        const numA = cardinalA[i] || 0;
-        const numB = cardinalB[i] || 0;
-        if (numA !== numB) {
-          return numB - numA;
-        }
-      }
-
-      return 0;
-    });
-
-    setFilteredMembers(sortedMembers);
   };
-
   return (
     <SortButtonWrapper onClick={onClickSort}>
       {isAscending ? '오름차순' : '기수 순'}
