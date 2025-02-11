e4bc0f692714be14741ebc4c0309036454541783
JIN921 <wlslwlsl808@gmail.com>
2025-02-12 01:21:00 +0900

    refactor: 홈 유저 정보 컴포넌트 분리

---
+++ src/components/home/HomeInfo.tsx
@@ -0,0 +1,41 @@
+import * as S from '@/styles/home/HomeMain.styled';
+import useGetUserInfo from '@/api/useGetUserInfo';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import Caption from '@/components/Button/Caption';
+
+const UserCharacter = styled.img``;
+const HomeInfo = () => {
+  const navi = useNavigate();
+  const { userInfo } = useGetUserInfo();
+
+  const userName = userInfo?.name || 'Loading';
+  const cardinal =
+    Array.isArray(userInfo?.cardinals) && userInfo?.cardinals.length >= 2
+      ? userInfo.cardinals[userInfo.cardinals.length - 1]
+      : userInfo?.cardinals?.[0] || '...';
+  return (
+    <S.StyledHomeMain>
+      <S.CaptionContainer>
+        <Caption color="#ffffff" textcolor="#000000">
+          {cardinal}기
+        </Caption>
+      </S.CaptionContainer>
+      <S.UserInfo>
+        <S.UserContainer>
+          <S.Name>{userName}</S.Name>
+          <S.NickName>Elite님</S.NickName>
+        </S.UserContainer>
+        <S.RightButtonContainer>
+          <UserCharacter
+            onClick={() => {
+              navi(`/mypage`);
+            }}
+          />
+        </S.RightButtonContainer>
+      </S.UserInfo>
+    </S.StyledHomeMain>
+  );
+};
+
+export default HomeInfo;
--- src/components/home/HomeMain.tsx
+++ src/components/home/HomeMain.tsx
@@ -1,44 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 
-import Caption from '@/components/Button/Caption';
-import RightButton from '@/components/Header/RightButton';
-
 import calendar from '@/assets/images/ic_home_calendar.svg';
 import attend from '@/assets/images/ic_home_attend.svg';
 import board from '@/assets/images/ic_home_board.svg';
 import * as S from '@/styles/home/HomeMain.styled';
-import useGetUserInfo from '@/api/useGetUserInfo';
 
 const HomeMain: React.FC = () => {
   const navi = useNavigate();
-  const { userInfo } = useGetUserInfo();
-
-  const userName = userInfo?.name || 'Loading';
-  const cardinal =
-    Array.isArray(userInfo?.cardinals) && userInfo?.cardinals.length >= 2
-      ? userInfo.cardinals[userInfo.cardinals.length - 1]
-      : userInfo?.cardinals?.[0] || 'Loading';
 
   return (
     <S.StyledHomeMain>
-      <S.CaptionContainer>
-        <Caption color="#ffffff" textcolor="#000000">
-          {cardinal}기
-        </Caption>
-      </S.CaptionContainer>
-      <S.UserInfo>
-        <S.UserContainer>
-          <S.Name>{userName}</S.Name>
-          <S.NickName>Elite님</S.NickName>
-        </S.UserContainer>
-        <S.RightButtonContainer>
-          <RightButton
-            onClick={() => {
-              navi(`/mypage`);
-            }}
-          />
-        </S.RightButtonContainer>
-      </S.UserInfo>
       <S.GridContainer>
         <S.CalendarItem
           onClick={() => {
