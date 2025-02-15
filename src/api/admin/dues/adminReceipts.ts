9d98e603b9d3cb2353191855ce01938d2e11f453
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-16 02:14:34 +0900

    fix: api 인스턴스 연결

--- src/api/admin/dues/adminReceipts.ts
+++ src/api/admin/dues/adminReceipts.ts
@@ -1,19 +1,9 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_API_URL;
-const ACCESSTOKEN = import.meta.env.VITE_MASTER_TOKEN;
+import api from '@/api/api';
 
 const adminReceipts = async (data: any) => {
   try {
-    const response = await axios.post(
-      `${BASE_URL}/api/v1/admin/receipts`,
-      data,
-      {
-        headers: {
-          Authorization: `Bearer ${ACCESSTOKEN}`,
-        },
-      },
-    );
+    const response = await api.post(`/api/v1/admin/receipts`, data);
+    console.log('?', response);
     return response.data;
   } catch (error: any) {
     throw new Error(
--- src/api/admin/dues/upLoadFiles.ts
+++ src/api/admin/dues/upLoadFiles.ts
@@ -1,15 +1,9 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_API_URL;
-const ACCESSTOKEN = import.meta.env.VITE_MASTER_TOKEN;
+import api from '@/api/api';
 
 const getFileUrl = async (data: File, fileName: string) => {
-  const res = await axios.get(`${BASE_URL}/files/`, {
+  const res = await api.get(`/files/`, {
     params: { fileName },
     data,
-    headers: {
-      Authorization: `Bearer ${ACCESSTOKEN}`,
-    },
   });
   return res.data;
 };
