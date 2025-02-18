858d77e1c84c0c04f3d6797273913041c79643a9
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-19 02:17:18 +0900

    fix: 기수 드롭다운 사용자 table border 수정

--- src/styles/admin/Attendance.styled.ts
+++ src/styles/admin/Attendance.styled.ts
@@ -8,7 +8,9 @@ export const AttendanceTable = styled.div`
   display: flex;
   margin-left: 2.5%;
   margin-top: 15px;
-  border-bottom: 1px solid #dedede;
+  border-collapse: collapse;
+  border-spacing: 0;
+  cursor: pointer;
 
   &:last-child {
     margin-bottom: 15px;
@@ -40,6 +42,7 @@ export const ContentText = styled.span`
   color: black;
 `;
 
-export const Button = styled.div`
-  cursor: pointer;
+export const DropdownButton = styled.img<{ isOpen: boolean }>`
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? 'rotate(180deg)' : 'rotate(0)')};
 `;
