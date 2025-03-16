374d3673107d5a075e59c8ce9fe67c1d9c793f53
JIN921 <wlslwlsl808@gmail.com>
2025-03-16 18:29:53 +0900

    fix: 메뉴 모달 레귤러 폰트 제거

--- src/components/common/MenuModal.tsx
+++ src/components/common/MenuModal.tsx
@@ -33,7 +33,6 @@ const Content = styled.div`
   border-radius: 10px;
   font-size: 14px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  font-family: ${theme.font.regular};
   cursor: pointer;
 `;
 
