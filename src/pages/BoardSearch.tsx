04e24e5dcc2c0e28fb0e583d0d25c1db8067d5e5
JIN921 <wlslwlsl808@gmail.com>
2025-04-24 13:25:27 +0900

    feat: 게시판 검색 페이지 생성

--- src/pages/BoardSearch.tsx
+++ src/pages/BoardSearch.tsx
@@ -1,23 +1,9 @@
-import CardinalDropdown from '@/components/common/CardinalDropdown';
 import Header from '@/components/Header/Header';
-import MemberList from '@/components/Member/MemberList';
-import useCustomBack from '@/hooks/useCustomBack';
 import theme from '@/styles/theme';
-import { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
 import styled from 'styled-components';
 import search from '@/assets/images/ic_search.svg';
-import getSearchMember from '@/api/getSearchMember';
-import { User } from '@/types/user';
 import { toastError } from '@/components/common/ToastMessage';
-
-const Wrapper = styled.div`
-  width: 370px;
-  margin-bottom: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 const Search = styled.div`
   display: flex;
@@ -49,25 +35,16 @@ export const SearchInput = styled.input`
 const SearchButton = styled.img`
   cursor: pointer;
 `;
-
-const Member = () => {
-  useCustomBack('/home');
-  const [searchParams] = useSearchParams();
-  const cardinal = searchParams.get('cardinal');
-  const [selectedCardinal, setSelectedCardinal] = useState<number | null>(
-    Number(cardinal) || null,
-  );
+const BoardSearch = () => {
   const [keyword, setKeyword] = useState<string>('');
-  const [searchResults, setSearchResults] = useState<User[] | undefined>([]);
+  const [searchResults, setSearchResults] = useState();
   const [loading, setLoading] = useState<boolean>(false);
-  const navigate = useNavigate();
 
   const handleSearch = async () => {
     setLoading(true);
     try {
       const response = await getSearchMember(keyword);
       setSearchResults(response.data.data);
-      navigate(`/member?search=${keyword}`);
     } catch (error) {
       toastError('데이터를 불러오지 못했습니다.');
       // eslint-disable-next-line no-console
@@ -77,38 +54,28 @@ const Member = () => {
     }
   };
 
-  useEffect(() => {
-    navigate(`/member?cardinal=${selectedCardinal}`);
-  }, [selectedCardinal]);
-
   const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       handleSearch();
     }
   };
-
   return (
-    <Wrapper>
+    <div>
       <Header RightButtonType="none" isAccessible>
-        멤버
+        게시판 검색
       </Header>
       <Search>
-        <CardinalDropdown
-          origValue={selectedCardinal}
-          editValue={setSelectedCardinal}
-          isMember
-        />
         <SearchInput
-          placeholder="멤버 이름을 검색하세요"
+          placeholder="제목, 내용 검색"
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
           onKeyDown={handleEnter}
         />
         <SearchButton src={search} alt={search} onClick={handleSearch} />
       </Search>
-      <MemberList searchResults={searchResults} loading={loading} />
-    </Wrapper>
+      <SearchList searchResults={searchResults} loading={loading} />
+    </div>
   );
 };
 
-export default Member;
+export default BoardSearch;
